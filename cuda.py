3779299d0dbf4889cbb1fc69ef1c9e122b13c16e
rtous <rtous@ac.upc.edu>
2021-09-29 15:51:57 +0200

    nc

--- cuda.py
+++ cuda.py
@@ -8,6 +8,6 @@ print("torch.cuda.device_count():", torch.cuda.device_count())
 
 print("torch.cuda.get_device_name(0):", torch.cuda.get_device_name(0))
 
-print("Suported architectures (compute capabiliies):", torch.cuda.get_arch_list())
+#print("Suported architectures (compute capabiliies):", torch.cuda.get_arch_list())
 
 print(torch.rand(2,3).cuda())
