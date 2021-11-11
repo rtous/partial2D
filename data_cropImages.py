8758ef463eac6f81e6144c217b43c95f3cc4b6d4
Ruben Tous <ruben.tous@adsmurai.com>
2021-11-11 09:24:43 +0100

    nc

--- data_cropImages.py
+++ data_cropImages.py
@@ -9,8 +9,11 @@ from os.path import isfile, join, splitext
 import pathlib
 import traceback
 
-INPUTPATH = "data/H36M_ECCV18/images"
-OUTPUTPATH = "data/H36M_ECCV18/images_cropped"
+#INPUTPATH = "data/H36M_ECCV18/images"
+#OUTPUTPATH = "data/H36M_ECCV18/images_cropped"
+
+INPUTPATH = "data/H36M_ECCV18/Test/IMG"
+OUTPUTPATH = "data/H36M_ECCV18/Test/IMG_CROPPED"
 
 pathlib.Path(OUTPUTPATH).mkdir(parents=True, exist_ok=True) 
 
@@ -19,7 +22,7 @@ files = [f for f in listdir(INPUTPATH) if isfile(join(INPUTPATH, f))]
 for filename in files:
 	img = cv2.imread(join(INPUTPATH, filename))
 	height, width, channels = img.shape
-	cv2.rectangle(img, (0, int(height/1.5)), (width, height), (0,0,0), -1)
+	cv2.rectangle(img, (0, int(height/2.2)), (width, height), (0,0,0), -1)
 	cv2.imwrite(join(OUTPUTPATH, filename), img)
 
 
