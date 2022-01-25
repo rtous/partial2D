366b3f7fed25d75ecf96c4f58491595b98a46da5
Ruben Tous <ruben.tous@adsmurai.com>
2022-01-25 08:42:35 +0100

    nc

--- train_asterixECCV2018.sh
+++ train_asterixECCV2018.sh
@@ -5,7 +5,7 @@ OUTPUTPATH="data/output/ECCV2018"
 dataroot_validation="/home/users/jpoveda/charade/input/keypoints"
 TEST_IMAGES_PATH="/home/users/jpoveda/charade/input/images"
 
-
-nohup python test12_poseCompletion4_CGAN_v3_justpatch_charade.py $dataroot_cropped $dataroot_original $OUTPUTPATH $dataroot_validation $TEST_IMAGES_PATH &
+python test12_poseCompletion4_CGAN_v3_justpatch_charade.py $dataroot_cropped $dataroot_original $OUTPUTPATH $dataroot_validation $TEST_IMAGES_PATH
+#nohup python test12_poseCompletion4_CGAN_v3_justpatch_charade.py $dataroot_cropped $dataroot_original $OUTPUTPATH $dataroot_validation $TEST_IMAGES_PATH &
 #nohup python test12_poseCompletion4_CGAN_v3_justpatch_charade.py $dataroot_cropped $dataroot_original $OUTPUTPATH $dataroot_validation $TEST_IMAGES_PATH >nohup.out 2>&1 &
 
